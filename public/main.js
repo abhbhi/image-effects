47eec34585d340a0d7fd2e99dd4150f62780a9e0
Avinash <avinashsharma8907@gmail.com>
2022-10-09 16:18:01 +0530

    dev done

--- public/main.js
+++ public/main.js
@@ -1,5 +1,12 @@
-
-function init() {
+async function init() {
+    let rustApp=null
+    try{
+        rustApp=await import ('../pkg')
+    } catch(e){
+        console.error(e)
+        return
+    }
+    console.log(rustApp)
     const input = document.getElementById('upload')
     const fileReader = new FileReader()
 
@@ -7,7 +14,10 @@ function init() {
         const base64 = fileReader.result.replace(
             /^data:image\/(png|jpeg|jpg);base64,/, ''
         );
-        console.log(base64)
+        let img_data_url=rustApp.grayscale(base64)
+        document.getElementById('new-img').setAttribute(
+            'src',img_data_url
+        )
     }
 
     input.addEventListener('change', () => {
